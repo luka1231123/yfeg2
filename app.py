1e124e9c88e604fa64518ed1e3cef07e08cb7b6a
luka1231123 <luka04rekhviashvili@gmail.com>
2024-08-06 13:14:09 +0400

    modified:   app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy # type: ignore
 from datetime import datetime
 import smtplib
 from email.mime.text import MIMEText
